a3f63abf787b5cdbd9e7a229af1a5baa252f9487
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2021-02-09 12:26:13 -0600

    Creation of project and resources

---
+++ app/src/main/java/com/example/junit_course/ui/note/NoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.junit_course.ui.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.junit_course.R;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
